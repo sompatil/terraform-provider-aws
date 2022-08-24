38d3e2832d669e176a30c9150dba19c9a1bfcde5
sompatil <105210730+sompatil@users.noreply.github.com>
2022-08-24 11:53:15 +0530

    Update eks-cluster.tf

--- examples/eks-getting-started/eks-cluster.tf
+++ examples/eks-getting-started/eks-cluster.tf
@@ -64,7 +64,7 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
 resource "aws_eks_cluster" "demo" {
   name     = var.cluster-name
   role_arn = aws_iam_role.demo-cluster.arn
-
+  version  = "1.20"
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
     subnet_ids         = aws_subnet.demo[*].id
